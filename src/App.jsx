319874a63e00eb17dea7902e2835dfa873fe17a6
Julius Lam <julius.lam2@gmail.com>
2026-03-04 22:28:04 +0100

    Add navbar and remove more fluff

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/navbar' 
 
 function App() {
 
   return (
     <>
       <div>
-        Julius Lam
+        <Navbar></Navbar>
       </div>
     </>
   )
---
+++ src/components/navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './navbar.css'
+
+function Navbar() {
+    return(
+        <nav className="navbar">
+            <div className="navbar-container">
+                <div>
+                    Julius Lam
+                </div>
+                <div>
+                    ...
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
