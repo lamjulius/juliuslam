a632e821acee1c0db1752edfcbcb15cc726822e2
Julius Lam <julius.lam2@gmail.com>
2026-03-06 23:19:04 +0100

    Add lucide logos + styling

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,9 +1,25 @@
+import { Linkedin, Github} from 'lucide-react';
 import './HeroSection.css' 
 
 function HeroSection() {
     return(
         <div className="HeroSection">
-            Julius Lam
+            <div className='HeroName'>
+                Julius Lam
+            </div>
+            <div className='HeroLogos'>
+                <a href="https://www.linkedin.com/in/lamjulius/"
+                   target="_blank"
+                   rel="noopener noreferrer">
+                    <Linkedin></Linkedin> 
+                </a>
+                <a href="https://github.com/lamjulius"
+                   target="_blank"
+                   rel="noopener noreferrer">
+                    <Github></Github>
+                </a>
+            </div>
+            
         </div>
     )
 }
